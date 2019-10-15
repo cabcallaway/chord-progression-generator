6670d464e18c3a2961b3b0e7d2a7404caba0316a
Cab Callaway <callaway.25@osu.edu>
2019-10-14 21:20:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/cpg/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.cpg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.ContentApi;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.CallResult;
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.ListItem;
+import com.spotify.protocol.types.ListItems;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "4d7413a14a664db8a826bb9735292790";
+    private static final String REDIRECT_URI = "com.example.cpg://callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+    private Button mPlayButton;
+    private Button mPauseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MainActivity","ONCREATE TRIGGERED");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPlayButton = (Button) findViewById(R.id.play_button);
+        mPauseButton = (Button) findViewById(R.id.pause_button);
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Play a playlist
+                mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+            }
+        });
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSpotifyAppRemote.getPlayerApi().pause();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("MainActivity","ONSTART TRIGGERED");
+        super.onStart();
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MainActivity", throwable.getMessage(), throwable);
+
+                        Log.d("MainActivity", "FAILED");
+                    }
+                });
+    }
+
+    private void connected() {
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                    }
+                });
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("MainActivity", "ONSTOP TRIGGERED");
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+}
