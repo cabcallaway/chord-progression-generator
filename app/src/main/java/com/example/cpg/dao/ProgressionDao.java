64089e95ab52152ed8bab2c98906ab51af1137fa
Morris <progmorris20@gmail.com>
2019-11-05 15:54:44 -0500

    modify ProgressionDao

--- app/src/main/java/com/example/cpg/dao/ProgressionDao.java
+++ app/src/main/java/com/example/cpg/dao/ProgressionDao.java
@@ -3,20 +3,27 @@ package com.example.cpg.dao;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
 import androidx.room.Update;
 
+import com.example.cpg.model.Chord;
 import com.example.cpg.model.Progression;
+
+import java.util.ArrayList;
 
 @Dao
 public interface ProgressionDao {
-/*
+
     @Insert
-    public void insert(Progression... progressions);
+    public void insert(Progression progression);
 
     @Update
-    public void update(Progression... progressions);
+    public void update(Progression progression);
 
     @Delete
-    public void delete(Progression);
-*/
+    public void delete(Progression progression);
+
+    @Query("SELECT chords FROM Progression WHERE id = :id")
+    public ArrayList<Chord> getChordsById(int id);
+
 }
